4548a03e2dd42cb5e39ebb43151f4a1ee7669906
Eden Alem <edenberhanalem@gmail.com>
2021-08-24 23:30:24 -0400

    Solve intersection of two arrays https://leetcode.com/problems/intersection-of-two-arrays/

---
+++ Week 2/IntersectionOfTwoArrays.py
@@ -0,0 +1,8 @@
+from typing import List
+
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        return list(set(nums1) & set(nums2))
+
+print(Solution().intersection([4,5,4],[4,6]))
+        

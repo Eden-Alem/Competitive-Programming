f6ac4cb42a834837148a8d6e01537ebbcbb724ce
Eden Alem <edenberhanalem@gmail.com>
2021-12-09 16:40:45 -0500

    Solve smaller than current number on leetcode https://leetcode.com/problems/how-many-numbers-are-smaller-than-the-current-number/

---
+++ Week 1/SmallerThanCurrent.py
@@ -0,0 +1,10 @@
+class Solution:
+    def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+        smaller = [0] * len(nums)
+        
+        for i in range(len(nums)):
+            for j in range(len(nums)):
+                if ((i != j) and (nums[j] < nums[i])):
+                    smaller[i] += 1
+        
+        return smaller

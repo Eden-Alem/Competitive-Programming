5659e58d91dd02f155c20e57e388e60c356b2b91
Eden Alem <edenberhanalem@gmail.com>
2021-12-09 13:51:52 -0500

    Updated bubble sort to include swap numbers from hackerrank https://www.hackerrank.com/challenges/ctci-bubble-sort/problem

--- Week 1/BubbleSort.py
+++ Week 1/BubbleSort.py
@@ -4,3 +4,40 @@ def bubbleSort(array):
             if (array[j] > array[j+1]):
                 array[j], array[j+1] = array[j+1], array[j]
 
+
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countSwaps' function below.
+#
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def countSwaps(a):
+    # Write your code here
+    noOfSwaps = 0
+    
+    for i in range(len(a)):
+        for j in range(len(a) - i - 1):
+            if (a[j] > a[j+1]):
+                a[j], a[j+1] = a[j+1], a[j]
+                noOfSwaps += 1
+                
+    print(f"Array is sorted in {str(noOfSwaps)} swaps.")
+    print(f"First Element: {str(a[0])}")
+    print(f"Last Element: {str(a[len(a) - 1])}")
+    
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    countSwaps(a)

a8b92d1400e71f7c238f9ffcdbf035a6c81f3d36
Eden Alem <edenberhanalem@gmail.com>
2021-12-17 13:30:14 -0500

    Updated minstack on leetcode

--- Week 2/MinStack.py
+++ Week 2/MinStack.py
@@ -5,17 +5,32 @@ class MinStack:
         initialize your data structure here.
         """
         self.minStack = []
-        
+        self.stack = []
 
     def push(self, val: int) -> None:
-        self.minStack.append(val)
+        self.stack.append(val)
+        if (len(self.minStack) == 0):
+            self.minStack.append(val)
+        else:
+            if (self.minStack[-1] > self.stack[-1]):
+                self.minStack.append(self.stack[-1])
+            else:
+                self.minStack.append(self.minStack[-1])
 
     def pop(self) -> None:
         self.minStack.pop()
+        self.stack.pop()
 
     def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:        
         return self.minStack[-1]
 
-    def getMin(self) -> int:
-        return min(self.minStack)
 
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()

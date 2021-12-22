11c94687c6c5d95afccd379e7238acccd92f8a68
Eden Alem <edenberhanalem@gmail.com>
2021-12-22 00:31:05 -0500

    Solve longest subarray on leetcode https://leetcode.com/problems/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/

---
+++ Week 2/longestSubarray.py
@@ -0,0 +1,30 @@
+from typing import Deque
+
+
+class Solution:
+    def longestSubarray(self, nums, limit: int) -> int:
+        left = 0
+        right = 0
+        
+        incDeq = Deque()
+        decDeq = Deque()
+        
+        result = 0
+        while (right < len(nums)):
+            while (len(incDeq) and nums[right] <= nums[incDeq[-1]]):
+                incDeq.pop()
+            incDeq.append(right)
+                
+            while (len(decDeq) and nums[right] >= nums[decDeq[-1]]):
+                decDeq.pop()
+            decDeq.append(right)
+            
+            if (abs(nums[decDeq[0]] - nums[incDeq[0]]) > limit):
+                left += 1
+                if (left > decDeq[0]): decDeq.popleft()
+                if (left > incDeq[0]): incDeq.popleft()
+            else:
+                result = max(result, right-left+1)
+                right += 1
+            
+        return result

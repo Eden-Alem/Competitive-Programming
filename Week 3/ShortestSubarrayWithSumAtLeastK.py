c9c5d94304414070672d2d982865571fb2a84c25
Eden Alem <edenberhanalem@gmail.com>
2021-12-24 05:06:40 -0500

    Solve shortest subarray with sum atleast k on leetcode https://leetcode.com/problems/shortest-subarray-with-sum-at-least-k/

---
+++ Week 3/ShortestSubarrayWithSumAtLeastK.py
@@ -0,0 +1,21 @@
+from typing import Deque
+
+
+class Solution:
+    def shortestSubarray(self, nums, k: int) -> int:
+        prefixSums = [0] * (len(nums) + 1)
+        
+        for i in range(len(nums)):
+            prefixSums[i+1] = prefixSums[i] + nums[i]
+        
+        result = float('inf')
+        monoQueue = Deque()
+        for i in range(len(prefixSums)):
+            while (len(monoQueue) and prefixSums[i] <= prefixSums[monoQueue[-1]]):
+                monoQueue.pop()
+            monoQueue.append(i)
+            
+            while (len(monoQueue) and prefixSums[i] - prefixSums[monoQueue[0]] >= k):
+                result = min(result, i - monoQueue.popleft())
+         
+        return -1 if (result == float('inf')) else result

b5f6e583911eb58ca4e58c7a1c3f2ccbdaf9c1da
Eden Alem <edenberhanalem@gmail.com>
2022-05-20 18:45:25 +0300

    Time: 583 ms (5.78%), Space: 21.4 MB (67.97%) - LeetHub

--- 89-gray-code/89-gray-code.py
+++ 89-gray-code/89-gray-code.py
@@ -1,27 +1,18 @@
 class Solution:
     def grayCode(self, n: int) -> List[int]:
-        result = [0]
-        self.checkSequence(n, result, 0, set([0]))
-        return result
-        
-    def checkSequence(self, n, result, curr, visited):
-        if len(result) == 2**n:
-            return True
-        
-        
-        for i in range(n):            
-            val = curr ^ (1 << i)
+        result = [0]* (2**n)
+        for i in range(n):
+            temp = [0] * 2**i + [1] *  2**i
+            while len(temp) < 2**n:
+                temp = temp + list(reversed(temp))
             
-            if val not in visited:
-                visited.add(val)
-                result.append(val)
-                
-                if self.checkSequence(n, result, val, visited):
-                    return True
-                
-                visited.remove(val)
-                result.pop()
-                
-        return False
+            # result[0] = temp[0] xor result[0]
+            # result[1] = temp[1] xor result[1]
+            for j in range(len(temp)):
+                result[j] = temp[j]<<i ^ result[j]
+             
+        return result
+            
+            
+            
         
-                

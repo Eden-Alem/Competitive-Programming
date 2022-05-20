81270b565b9662fb05354bda53f4bb64138a93d5
Eden Alem <edenberhanalem@gmail.com>
2022-05-20 18:49:39 +0300

    Time: 340 ms (9.41%), Space: 21.3 MB (67.97%) - LeetHub

--- 89-gray-code/89-gray-code.py
+++ 89-gray-code/89-gray-code.py
@@ -3,11 +3,10 @@ class Solution:
         result = [0]* (2**n)
         for i in range(n):
             temp = [0] * 2**i + [1] *  2**i
-            while len(temp) < 2**n:
-                temp = temp + list(reversed(temp))
             
-            # result[0] = temp[0] xor result[0]
-            # result[1] = temp[1] xor result[1]
+            while len(temp) < 2**n:
+                temp.extend(temp[::-1])
+                
             for j in range(len(temp)):
                 result[j] = temp[j]<<i ^ result[j]
              

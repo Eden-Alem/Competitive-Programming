4cd09c08758b2358547d0a107756ae78859419dc
Eden Alem <edenberhanalem@gmail.com>
2022-06-06 10:27:12 +0300

    Time: 47 ms (91.58%), Space: 13.8 MB (99.13%) - LeetHub

--- 52-n-queens-ii/52-n-queens-ii.py
+++ 52-n-queens-ii/52-n-queens-ii.py
@@ -3,20 +3,24 @@ class Solution:
         self.result = 0
         
     def totalNQueens(self, n: int) -> int:
-        columns, leftDiag, rightDiag = set(), set(), set()
+        cols, leftDiag, rightDiag = set(), set(), set()
 		
         def backtrack(r): 
             if r == n:
                 self.result += 1
                 return
+            
             for c in range(n):
-                if c in columns or (r - c) in leftDiag or (r + c) in rightDiag:
+                if c in cols or (r - c) in leftDiag or (r + c) in rightDiag:
                     continue
-                columns.add(c)
+                    
+                cols.add(c)
                 leftDiag.add(r - c)
                 rightDiag.add(r + c)
+                
                 backtrack(r + 1)
-                columns.remove(c)
+                
+                cols.remove(c)
                 leftDiag.remove(r - c)
                 rightDiag.remove(r + c)
             

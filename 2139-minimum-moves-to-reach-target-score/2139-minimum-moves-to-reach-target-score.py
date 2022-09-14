420887807a5d5dcb53e55547bcb9796779315419
Eden Alem <edenberhanalem@gmail.com>
2022-09-13 22:26:30 -0400

    Time: 56 ms (29.71%), Space: 13.9 MB (60.45%) - LeetHub

--- 2139-minimum-moves-to-reach-target-score/2139-minimum-moves-to-reach-target-score.py
+++ 2139-minimum-moves-to-reach-target-score/2139-minimum-moves-to-reach-target-score.py
@@ -1,18 +1,20 @@
 class Solution:
     def minMoves(self, target: int, maxDoubles: int) -> int:
+        
         count = 0
         
-        while target != 1:
-            if (target % 2 == 1):
-                target -= 1
-                count += 1
+        while target > 1 and maxDoubles > 0:
+            if (target % 2 == 0):
+                target //= 2
+                maxDoubles -= 1
+                
             else:
-                if (maxDoubles > 0):
-                    target //= 2
-                    maxDoubles -= 1
-                    count += 1
-                else:
-                    count += (target - 1)
-                    target = 1
-                    
-        return count
+                target -= 1
+            
+            count += 1
+                
+        
+        return count + (target-1)
+                
+
+        

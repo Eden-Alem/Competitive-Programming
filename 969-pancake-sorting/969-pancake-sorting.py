98a2b3b4f246f5509bbc31b5a56667f94f614e58
Eden Alem <edenberhanalem@gmail.com>
2023-02-08 11:30:14 -0500

    Time: 46 ms (62.78%), Space: 13.9 MB (24.01%) - LeetHub

---
+++ 969-pancake-sorting/969-pancake-sorting.py
@@ -0,0 +1,42 @@
+class Solution:
+    def pancakeSort(self, arr: List[int]) -> List[int]:
+        right = len(arr) - 1
+        
+        isSorted = True
+        for i in range(1,len(arr)):
+            if arr[i] < arr[i-1]:
+                isSorted = False
+                break
+        if isSorted: return []
+        
+        res = []
+        temp = arr[:right+1]
+        
+        while (right):
+            maxValIndex = max_index(temp)
+            
+            leftArray = temp[:maxValIndex+1]
+            if not maxValIndex == right:
+                rightArray = temp[maxValIndex+1:]
+                leftArray.reverse()
+                
+                res.append(maxValIndex+1)
+                leftArray.extend(rightArray)
+                leftArray.reverse()
+                
+                res.append(right+1)
+                
+            temp = leftArray[:right]
+            
+            right -= 1
+            
+        return res
+    
+
+def max_index(arr):
+    max_i = 0
+    for i in range(len(arr)):
+        if (arr[i] > arr[max_i]):
+            max_i = i
+
+    return max_i

8122985b590520489f2ff539f3a5dd2fe08ed8a9
Eden Alem <edenberhanalem@gmail.com>
2023-02-21 13:05:22 -0500

    Time: 52 ms (55.30%), Space: 13.8 MB (98.14%) - LeetHub

---
+++ 13-roman-to-integer/13-roman-to-integer.py
@@ -0,0 +1,16 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        symbols = {"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000}
+        
+        n = len(s)
+        result = 0
+        
+        for i in range(n):
+            if i+1 < n and symbols[s[i]] < symbols[s[i+1]]:
+                result -= symbols[s[i]]
+                
+            else:
+                result += symbols[s[i]]
+                
+        return result
+        

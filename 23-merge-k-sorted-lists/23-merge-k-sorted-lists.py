fa76964e5b9e46034ae3a38505836b5d15ef25b2
Eden Alem <edenberhanalem@gmail.com>
2023-02-24 13:01:35 -0500

    Time: 116 ms (51.90%), Space: 17.5 MB (90.45%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
@@ -5,17 +5,40 @@
 #         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        array = []
-        for head in lists:
-            while head:
-                heapq.heappush(array, head.val)
-                head = head.next
-                
-        node = dummy_node = ListNode()
-        while array:
-            a = heapq.heappop(array)
-            dummy_node.next = ListNode(a)
-            dummy_node = dummy_node.next
+        if not lists or len(lists) == 0:
+            return None
+        
+        while len(lists) > 1:
+            new_merged_lists = []
             
-        return node.next
+            for i in range(0, len(lists), 2):
+                l1 = lists[i]
+                l2 = lists[i+1] if (i+1)<len(lists) else None
+                
+                new_merged_lists.append(self.mergeList(l1,l2))
+                
+            lists = new_merged_lists
+            
+        return lists[0]
+            
+        
+    def mergeList(self, l1, l2):
+        dummy = head = ListNode()
+        
+        while l1 and l2:
+            if l1.val <= l2.val:
+                head.next = l1
+                l1 = l1.next
+                
+            else:
+                head.next = l2
+                l2 = l2.next
+                
+            head = head.next
+            
+        if l1: head.next = l1
+            
+        if l2: head.next = l2
+            
+        return dummy.next
         

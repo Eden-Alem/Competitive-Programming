c9e0e15b53dfae248fd4361b4b7776e8e9175eb6
Eden Alem <edenberhanalem@gmail.com>
2023-03-01 13:30:59 -0500

    Time: 448 ms (95.64%), Space: 32.4 MB (59.74%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.py
+++ 2540-minimum-common-value/2540-minimum-common-value.py
@@ -1,9 +1,18 @@
 class Solution:
     def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
-        nums2_uniq = set(nums2)
+        n1 = len(nums1)
+        n2 = len(nums2)
         
-        for n in nums1:
-            if n in nums2_uniq:
-                return n
+        p1, p2 = 0, 0
+        
+        while p1 <= n1-1 and p2 <= n2-1:
+            if nums1[p1] == nums2[p2]:
+                return nums1[p1]
+            
+            elif nums1[p1] < nums2[p2]:
+                p1 += 1
+                
+            else:
+                p2 += 1
         
         return -1

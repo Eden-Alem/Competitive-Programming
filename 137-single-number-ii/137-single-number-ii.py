33153d62b3c72c5aaf186007bac421fdd643c734
Eden Alem <edenberhanalem@gmail.com>
2023-05-22 08:53:51 -0400

    Time: 68 ms (60.42%), Space: 18.4 MB (7.66%) - LeetHub

---
+++ 137-single-number-ii/137-single-number-ii.py
@@ -0,0 +1,10 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        freq = Counter(nums)
+        
+        for num in freq:
+            if freq[num] == 1:
+                return num
+            
+            
+        
